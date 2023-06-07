43b15e726ae3848286695c16b79dc77b858eb484
Merel <merel@wemel.nl>
2023-06-07 12:46:26 +0100

    Updated settings for HPC running

--- settings.py
+++ settings.py
@@ -6,12 +6,13 @@ class Settings():
 
     def __init__(self):
 
-        self.home_dir = "C:/Users/Merel/Documents/Sanquin/Projects/RBC matching/Sanquin_NHSBT_bloodmatching/"
+        # self.home_dir = "C:/Users/Merel/Documents/Sanquin/Projects/RBC matching/Sanquin_NHSBT_bloodmatching/"
+        self.home_dir = "/home/mw922/Sanquin_NHSBT_bloodmatching/"
 
         # "demand": generate demand data
         # "supply": generate supply data
         # "optimize": run simulations and optimize matching
-        self.mode = "optimize"
+        self.mode = "demand"
 
         # Output files will be stored in directory results/[model_name].
         self.model_name = "LHD testing"
@@ -38,16 +39,16 @@ class Settings():
 
         # (x,y): Episode numbers range(x,y) will be optimized.
         # The total number of simulations executed will thus be y - x.
-        self.episodes = (10,20)
+        self.episodes = (0,1)
 
         # Number of hospitals considered. If more than 1 (regional and university combined), a distribution center is included.
         # "UCLH" : University College London Hospitals
         # "NMUH" : North Middlesex University Hospital
         # "WH" : Whittington Health
         self.n_hospitals = {
-            "UCLH" : 0,
+            "UCLH" : 1,
             "NMUH" : 0,
-            "WH" : 1,
+            "WH" : 0,
         }
 
         # Size factor for distribution center and hospitals.
