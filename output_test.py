26f1e5dc7661225a31b62ed814794478e72b9a96
Merel <merel@wemel.nl>
2023-06-12 05:50:27 +0100

    Test format of output

---
+++ output_test.py
@@ -0,0 +1,13 @@
+HOME_DIR = "/home/mw922/Sanquin_NHSBT_bloodmatching/"
+episode = 0
+htype = "UCLH"
+init_days = 2 * 35
+test_days = 14
+
+for day in range(init_days + test_days):
+    print(unpickle(HOME_DIR + f"{log_type}/{folders[0]}/{episode}/patients_patgroups_{htype}/{day}"))
+
+
+def unpickle(path):
+    with open(path+".pickle", 'rb') as f:
+        return pickle.load(f)
