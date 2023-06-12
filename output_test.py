35f8be7780978e709f259fa6f1dfe6a0dea9e573
Merel <merel@wemel.nl>
2023-06-12 05:52:28 +0100

    bugfix

--- output_test.py
+++ output_test.py
@@ -1,5 +1,6 @@
 HOME_DIR = "/home/mw922/Sanquin_NHSBT_bloodmatching/"
 episode = 0
+folder = "LHD testing"
 htype = "UCLH"
 init_days = 2 * 35
 test_days = 14
@@ -9,4 +10,4 @@ def unpickle(path):
         return pickle.load(f)
 
 for day in range(init_days + test_days):
-    print(unpickle(HOME_DIR + f"{log_type}/{folders[0]}/{episode}/patients_patgroups_{htype}/{day}"))
+    print(unpickle(HOME_DIR + f"results/{folder}/{episode}/patients_patgroups_{htype}/{day}"))
