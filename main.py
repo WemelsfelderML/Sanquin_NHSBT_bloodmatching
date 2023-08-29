e67875aeb3b33e1ced843189d8529b220a9b11ee
Merel <merel@wemel.nl>
2023-08-29 11:15:06 +0100

    bugfix

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ def main():
     # dir1 = f"{SETTINGS.LHD_configs}x{round((SETTINGS.episodes[1]-SETTINGS.episodes[0])/SETTINGS.LHD_configs)}LHD"
     dir1 = SETTINGS.dir1
 
-    dir2 = SETTINGS.model_name+"_" if SETTINGS.model_name != "" and SETTINGS.method == "BO" else ""
+    # dir2 = SETTINGS.model_name+"_" if SETTINGS.model_name != "" and SETTINGS.method == "BO" else ""
+    dir2 = SETTINGS.model_name+"_" if SETTINGS.model_name != "" else ""
     dir2 += "_".join(["".join([s[0] for s in obj_name.split("_")]) for obj_name in SETTINGS.n_obj.keys() if SETTINGS.n_obj[obj_name] > 0]) if SETTINGS.method == "BO" else ""
     dir2 += "/" if dir2 != "" else ""
 
