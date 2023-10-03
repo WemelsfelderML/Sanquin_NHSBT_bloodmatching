489ea23316c5f1da4709a2798b841fc13e09a66e
Merel <merel@wemel.nl>
2023-10-03 10:03:08 +0100

    baseline1

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings():
         self.mode = "optimize"
 
         # Output files will be stored in directory results/[model_name].
-        self.model_name = "baseline0"
+        self.model_name = "baseline1"
 
         
         ##########
