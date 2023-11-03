76ed9e7ada1973844a9182c5d738f426d33725b8
Merel <merel@wemel.nl>
2023-11-03 08:26:21 +0100

    bash file

--- slurm_job.sh
+++ slurm_job.sh
@@ -6,7 +6,7 @@
 
 #! How many nodes should be allocated? If not specified SLURM assumes 1 node.
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=1
 
 #! How many tasks will there be in total? By default SLURM will assume 1 task per node and 1 CPU per task.
 #SBATCH --ntasks=500
@@ -20,9 +20,16 @@
 #SBATCH --time=1:00:00
 
 #! Run your script with different arguments
-srun python main.py --model_name "ranged" --LHD_configs 100 --emin 0 --emax 100 --total_cores_max 8 &
-srun python main.py --model_name "newnew" --LHD_configs 500 --emin 0 --emax 500 --total_cores_max 8 &
+#! srun python main.py --model_name "ranged" --LHD_configs 100 --emin 0 --emax 100 --total_cores_max 8 &
+#! srun python main.py --model_name "newnew" --LHD_configs 500 --emin 0 --emax 500 --total_cores_max 8 &
 #! srun git add -f results &
 #! srun git add -f optimize_params &
 
+for i in {0..499}; do
+  emin=$i
+  emax=$((i+1))
+  srun python main.py --model_name "newnew" --LHD_configs 500 --emin "$emin" --emax "$emax" --total_cores_max 1 &
+done
+
 wait
+
