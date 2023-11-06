60e1cbadbddd2603a7ec50a5f7e67e19f039e0ce
Merel <merel@wemel.nl>
2023-11-06 11:19:25 +0100

    requesting 16 cores on node

--- slurm_job.sh
+++ slurm_job.sh
@@ -14,7 +14,7 @@
 
 #! How many tasks will there be in total? By default SLURM will assume 1 task per node and 1 CPU per task.
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=38
+#SBATCH --cpus-per-task=16
 
 #! How much memory in MB is required per node? Not setting this as here will lead to a default amount per task.
 #! Setting a larger amount per task increases the number of CPUs.
