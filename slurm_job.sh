72e16bf147a47607d9c6a03d68931d571d21edf9
Merel <merel@wemel.nl>
2023-11-06 13:08:34 +0100

    node 384

--- slurm_job.sh
+++ slurm_job.sh
@@ -4,7 +4,7 @@
 #SBATCH -p icelake
 
 #SBATCH --job-name=BloodMatch_mw922
-#SBATCH --nodelist=cpu-q-150
+#SBATCH --nodelist=cpu-q-384
 
 #SBATCH --output=res_%j.out
 #SBATCH --error=res_%j.err
@@ -14,7 +14,7 @@
 
 #! How many tasks will there be in total? By default SLURM will assume 1 task per node and 1 CPU per task.
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 
 #! How much memory in MB is required per node? Not setting this as here will lead to a default amount per task.
 #! Setting a larger amount per task increases the number of CPUs.
@@ -25,14 +25,14 @@
 
 #! Run your script with different arguments
 #! srun python main.py --model_name "ranged" --LHD_configs 100 --emin 0 --emax 100 --total_cores_max 8 &
-# srun python main.py --model_name "newnew" --LHD_configs 500 --emin 0 --emax 500 --total_cores_max 8 &
+#! srun python main.py --model_name "newnew" --LHD_configs 500 --emin 0 --emax 500 --total_cores_max 8 &
 srun python main.py &
 
-# for i in {0..499}; do
-#   emin=$i
-#   emax=$((i+1))
-#   srun python main.py --model_name "newnew" --LHD_configs 500 --emin "$emin" --emax "$emax" --total_cores_max 1 &
-# done
+#! for i in {0..499}; do
+#!   emin=$i
+#!   emax=$((i+1))
+#!   srun python main.py --model_name "newnew" --LHD_configs 500 --emin "$emin" --emax "$emax" --total_cores_max 1 &
+#! done
 
 wait
 
