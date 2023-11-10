185ef7fc064e3512600f0fbcd4e0945d83e5f210
Merel <merel@wemel.nl>
2023-11-10 05:48:27 +0000

    results

--- settings.py
+++ settings.py
@@ -54,9 +54,9 @@ class Settings():
         # (x,y): Episode numbers range(x,y) will be optimized.
         # The total number of simulations executed will thus be y - x.
         # self.episodes = (emin, emax)
-        self.episodes = (0, 500)
+        self.episodes = (300,400)
         # self.total_cores_max = total_cores_max    # Set the maximum number of cores to be used in total when executing episodes in parallel.
-        self.total_cores_max = 16    # Set the maximum number of cores to be used in total when executing episodes in parallel.
+        self.total_cores_max = 38    # Set the maximum number of cores to be used in total when executing episodes in parallel.
 
         # Number of hospitals considered. If more than 1 (regional and university combined), a distribution center is included.
         # "UCLH" : University College London Hospitals
