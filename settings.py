ea625122bbd514fc7923388ad96bde00fbadb8f8
Merel <merel@wemel.nl>
2023-11-21 09:19:21 +0100

    rm results + setup "cons" test

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ class Settings():
         self.mode = "optimize"
 
         # Output files will be stored in directory results/[model_name].
-        self.model_name = "dim5"
+        self.model_name = "cons"
 
         
         ##########
